514130e9511df64582fa6ec06eb2ed7af9f636c1
rifaa oumnia <rifaaoumnia@gmail.com>
2024-05-21 23:19:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/apprecipe/Listeners/RecipeClickListener.java
@@ -0,0 +1,5 @@
+package com.example.apprecipe.Listeners;
+
+public interface RecipeClickListener {
+    void onRecipeClicked(String id);
+}
---
+++ app/src/main/java/com/example/apprecipe/Models/Temperature.java
@@ -0,0 +1,6 @@
+package com.example.apprecipe.Models;
+
+public class Temperature {
+    public double number;
+    public String unit;
+}
